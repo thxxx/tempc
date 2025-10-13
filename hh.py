a02f7316581076ea19673f9970ab03bf1a00930a
Kim Hojin <zxcv05999@naver.com>
2025-10-14 02:39:41 +0900

    Update hh.py

--- hh.py
+++ hh.py
@@ -8,9 +8,15 @@ local_path = hf_hub_download(
     filename="first-project-438808-dc1804307b11.json"
 )
 
-
 # 원하는 위치로 복사 (이동하려면 shutil.move)
 target_path = "./first-project-438808-dc1804307b11.json"
 shutil.copy(local_path, target_path)
 
 print("Downloaded to:", local_path)
+
+local_json_path = hf_hub_download(
+    repo_id="Daniel777/personals",
+    filename="urls.json"
+)
+target_json_path = "./urls.json"
+shutil.copy(local_json_path, target_json_path)
